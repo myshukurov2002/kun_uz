6846842c5f15db20b9f127a2095a5e254eb40edd
Muhammad Yusuf <myshukurov@gmail.com>
2023-07-26 16:22:53 +0500

    comment 60%

---
+++ src/main/java/com/example/repository/CommentRepository.java
@@ -0,0 +1,1 @@
+package com.example.repository;import com.example.entity.CommentEntity;import org.springframework.data.jpa.repository.JpaRepository;public interface CommentRepository extends JpaRepository<CommentEntity, String> {}
