ea7db6a28393fec72c284a63d7fc62a3ec4ea8fa
Muhammad Yusuf <myshukurov@gmail.com>
2023-07-28 06:46:14 +0500

    CommentLike Entity created

---
+++ src/main/java/com/example/dto/CommentLikeDTO.java
@@ -0,0 +1,1 @@
+package com.example.dto;import com.fasterxml.jackson.annotation.JsonInclude;import lombok.Data;@Data@JsonInclude(JsonInclude.Include.NON_NULL)public class CommentLikeDTO {}
