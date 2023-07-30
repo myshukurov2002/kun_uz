79d551f6c37202aa4c95b31483e00fd2095faf12
Muhammad Yusuf <myshukurov@gmail.com>
2023-07-30 19:14:21 +0500

    ArticleLike ended

---
+++ src/main/java/com/example/repository/EmailHistoryRepository.java
@@ -0,0 +1,1 @@
+package com.example.repository;import com.example.entity.EmailHistory;import org.springframework.data.jpa.repository.JpaRepository;public interface EmailHistoryRepository extends JpaRepository<EmailHistory, String> {}
