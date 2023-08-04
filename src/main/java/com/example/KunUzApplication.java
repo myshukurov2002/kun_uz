cc81fee4199a357b44672798ec2c81f08534207f
Muhammad Yusuf <myshukurov@gmail.com>
2023-08-04 21:01:42 +0500

    sms 90%

--- src/main/java/com/example/KunUzApplication.java
+++ src/main/java/com/example/KunUzApplication.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import com.example.security.SpringConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SpringConfig.class)
 public class KunUzApplication {
 
     public static void main(String[] args) {
