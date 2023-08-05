6e58471a6fda4963aebcbbe49b60bd7ec00f5492
Muhammad Yusuf <myshukurov@gmail.com>
2023-08-05 15:23:47 +0500

    sms 100%

--- src/test/java/com/example/KunUzApplicationTests.java
+++ src/test/java/com/example/KunUzApplicationTests.java
@@ -2,8 +2,6 @@ package com.example;
 
 import com.example.entity.ProfileEntity;
 import com.example.util.JWTUtil;
-import com.example.util.MD5Util;
-import com.example.util.SecurityUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,7 +13,7 @@ class KunUzApplicationTests {
 
 //        System.out.println(MD5Util.encode("1"));
 
-        String token = JWTUtil.encode(20L, ProfileEntity.Role.MODERATOR);
+        String token = JWTUtil.encode(20L, ProfileEntity.Role.ROLE_MODERATOR);
         System.out.println(token);
         System.out.println(JWTUtil.decode(token));
 //        System.out.println(SecurityUtil.hasRole("Bearer " + token);
