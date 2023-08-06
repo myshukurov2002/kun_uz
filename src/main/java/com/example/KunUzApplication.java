9f5cc81af8ecefce8cfcfcf417cdf3c9cf788d89
Muhammad Yusuf <myshukurov@gmail.com>
2023-08-07 01:33:21 +0500

    CustomUserDetails, CustomUserDetailsService, SpringSecurityConfig ended

--- src/main/java/com/example/KunUzApplication.java
+++ src/main/java/com/example/KunUzApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(SpringConfig.class)
+//@Import({SpringConfig.class})
 public class KunUzApplication {
 
     public static void main(String[] args) {
